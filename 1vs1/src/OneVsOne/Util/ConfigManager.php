a615e86c78e0a71e3f1e6c6f67e29c579ab88a42
GamakCZ <gamak.mcpe@gmail.com>
2017-09-24 20:30:54 +0200

    Bug cloud not fixe :(

--- 1vs1/src/OneVsOne/Util/ConfigManager.php
+++ 1vs1/src/OneVsOne/Util/ConfigManager.php
@@ -46,7 +46,7 @@ class ConfigManager {
     public function loadArenas() {
         if(file_exists(self::getDataFolder()."arenas")) {
             foreach (glob(self::getDataFolder()."arenas/*.yml") as $arenaData) {
-                $this->plugin->getLogger()->debug("§aLoading {$arenaData} arena...");
+                $this->plugin->getLogger()->notice("§aLoading {$arenaData} arena...");
                 $this->loadArena($arenaData);
             }
         }
@@ -56,16 +56,36 @@ class ConfigManager {
      * @param string $fileName
      */
     public function loadArena(string $fileName) {
+
         $name = basename($fileName, ".yml");
+
         $config = new Config(self::getDataFolder()."arenas/{$name}.yml", Config::YAML);
+
         $pos1 = (array)$config->get("pos1");
+
+        if($this->plugin->getServer()->isLevelGenerated(strval($pos1[3]))) $this->plugin->getServer()->loadLevel(strval($pos1[3]));
+
         $pos2 = (array)$config->get("pos2");
+
+        if($this->plugin->getServer()->isLevelGenerated(strval($pos2[3]))) $this->plugin->getServer()->loadLevel(strval($pos2[3]));
+
         $signPos = (array)$config->get("signpos");
+
+        if($this->plugin->getServer()->isLevelGenerated(strval($signPos[3]))) $this->plugin->getServer()->loadLevel(strval($signPos[3]));
+
         $arena = $this->plugin->arenas[$name] = new Arena($this->plugin, $name, $this->plugin->getServer()->getDefaultLevel()->getSpawnLocation(), $this->plugin->getServer()->getDefaultLevel()->getSpawnLocation());
+
         $arena->name = $name;
-        $arena->pos1 = new Position(intval($pos1[0]), intval($pos1[1]), intval($pos1[2]), $this->plugin->getServer()->getLevelByName($pos1[3]));
-        $arena->pos2 = new Position(intval($pos2[0]), intval($pos2[1]), intval($pos2[2]), $this->plugin->getServer()->getLevelByName($pos2[3]));
+
+        $arena->pos1 = new Position(intval($pos1[0]), intval($pos1[1]), intval($pos1[2]), $this->plugin->getServer()->getLevelByName(strval($pos1[3])));
+
+        $arena->pos2 = new Position(intval($pos2[0]), intval($pos2[1]), intval($pos2[2]), $this->plugin->getServer()->getLevelByName(strval($pos2[3])));
+
         $arena->signpos = new Position(intval($signPos[0]), intval($signPos[1]), intval($signPos[2]), $this->plugin->getServer()->getLevelByName(strval($signPos[3])));
+
+        var_dump($arena);
+
+        sleep(10);
     }
 
     public function saveAll() {
